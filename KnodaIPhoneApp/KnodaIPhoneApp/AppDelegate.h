88311aea0ff76e7f77598f760e60c4811b5d6812
Helen Timofeeva <etimofeyeva@dataart.com>
2013-07-08 16:32:15 +0300

    Initial commit

---
+++ KnodaIPhoneApp/KnodaIPhoneApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KnodaIPhoneApp
+//
+//  Created by Elena Timofeeva on 7/8/13.
+//  Copyright (c) 2013 Knoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KnodaIPhoneApp/KnodaIPhoneApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KnodaIPhoneApp
+//
+//  Created by Elena Timofeeva on 7/8/13.
+//  Copyright (c) 2013 Knoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

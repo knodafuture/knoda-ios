e75f6fb4e4dd54f90370c0f5b360d5b65befa262
Vyacheslav Nechiporenko <vnechiporenko@abarantsevich2.kha.dataart.net>
2013-08-19 17:23:17 +0300

    BadgesCollectionViewController

---
+++ KnodaIPhoneApp/KnodaIPhoneApp/UI/BadgesCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  BadgesCollectionViewController.h
+//  KnodaIPhoneApp
+//
+//  Created by Vyacheslav Nechiporenko on 8/19/13.
+//  Copyright (c) 2013 Knoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BadgesCollectionViewController : UICollectionViewController
+
+@end

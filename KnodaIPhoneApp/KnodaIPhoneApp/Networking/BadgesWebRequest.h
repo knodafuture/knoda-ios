65e8223c002bb9a9a8665dcee53ca4ca18261e41
Vyacheslav Nechiporenko <vnechiporenko@abarantsevich2.kha.dataart.net>
2013-08-19 18:42:06 +0300

    BadgesWebRequest, showing user's badges

---
+++ KnodaIPhoneApp/KnodaIPhoneApp/Networking/BadgesWebRequest.h
@@ -0,0 +1,15 @@
+//
+//  BadgesWebRequest.h
+//  KnodaIPhoneApp
+//
+//  Created by Vyacheslav Nechiporenko on 8/19/13.
+//  Copyright (c) 2013 Knoda. All rights reserved.
+//
+
+#import "BaseWebRequest.h"
+
+@interface BadgesWebRequest : BaseWebRequest
+
+@property (nonatomic, strong) NSMutableArray *badgesImagesArray;
+
+@end

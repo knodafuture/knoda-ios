473e23279b484b764030d69c8bf954ee7fb1b60e
vlevschanov <vlevschanov@dataart.com>
2013-08-21 14:22:34 +0300

    Select picture screen refining

---
+++ KnodaIPhoneApp/KnodaIPhoneApp/Utils/UIImage+Utils.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Utils.h
+//  KnodaIPhoneApp
+//
+//  Created by Viktor Levschanov on 21.08.13.
+//  Copyright (c) 2013 Knoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Utils)
+
+- (UIImage *)scaledCroppedToSize:(CGSize)size;
+
+@end

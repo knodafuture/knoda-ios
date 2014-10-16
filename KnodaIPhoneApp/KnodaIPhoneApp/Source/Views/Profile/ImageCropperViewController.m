554b2a9cc4c2b798fbdc2bf8f1905f16aa6ce0ab
nick <nrellihan@gmail.com>
2014-10-16 14:48:01 -0500

    fixes #251

--- KnodaIPhoneApp/KnodaIPhoneApp/Source/Views/Profile/ImageCropperViewController.m
+++ KnodaIPhoneApp/KnodaIPhoneApp/Source/Views/Profile/ImageCropperViewController.m
@@ -19,10 +19,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
     self.cropView = [[BJImageCropper alloc] initWithImage:self.image andMaxSize:CGSizeMake(self.view.frame.size.width,
                                                                                            self.view.frame.size.height - 44.0/*nav bar height*/)];
-    self.cropView.crop   = CGRectMake(0, 0, self.cropSize.width, self.cropSize.height);    
+    self.cropView.crop   = CGRectMake(0, 0, self.cropSize.width, self.cropSize.height);
     self.cropView.center = CGPointMake(self.view.center.x, self.view.center.y - 100.0);
     
     self.navigationItem.leftBarButtonItem = [UIBarButtonItem styledBarButtonItemWithTitle:@"Cancel" target:self action:@selector(cancelButtonTapped:) color:[UIColor whiteColor]];
